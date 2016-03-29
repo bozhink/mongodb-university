9d3d63f045929e5c479b7a1d54bf9fc9b8211684
bozhink <bozhink@gmail.com>
2016-03-29 14:09:28 +0300

    GradeCleaner implemented. Need improvement.

---
+++ GradesCleaner/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace GradesCleaner
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.IO;
+    using MongoDB.Bson.Serialization;
+    using MongoDB.Driver;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            var client = new MongoClient();
+            var db = client.GetDatabase("st");
+            var collection = db.GetCollection<Student>("grades");
+
+            Console.WriteLine(collection.Count(new BsonDocument()));
+
+            var result = await collection.Find(s => s.Type == "homework")
+                .SortBy(s => s.StudentId)
+                .ThenBy(s => s.Score)
+                //.Skip(100)
+                //.Limit(12)
+                .ToListAsync();
+
+            result.ForEach(Console.WriteLine);
+
+            Console.WriteLine("----------------------------------------");
+
+            var itemsToDelete = result.GroupBy(s => s.StudentId)
+                .Select(g => g.FirstOrDefault());
+
+            foreach (var item in itemsToDelete)
+            {
+
+                Expression<Func<Student, bool>> filter = s => s.Id == item.Id;
+
+                (await collection.Find(filter).ToListAsync()).ForEach(Console.WriteLine);
+
+                await collection.DeleteOneAsync(filter);
+            }
+
+            Console.WriteLine(collection.Count(new BsonDocument()));
+        }
+    }
+}
---
+++ GradesCleaner/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace GradesCleaner
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public class Student
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("student_id")]
+        public int StudentId { get; set; }
+
+        [BsonElement("type")]
+        public string Type { get; set; }
+
+        [BsonElement("score")]
+        public double Score { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3}", this.Id, this.StudentId, this.Type, this.Score);
+        }
+    }
+}
