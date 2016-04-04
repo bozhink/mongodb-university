4291bf3e7fe05cbc4d5099b87a81185a8a9b12df
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-04 21:31:50 +0300

    M101N 2.3 is added.

---
+++ 2.3/src/M101DotNet.WebApp/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace M101DotNet.WebApp.Models
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+    }
+}
