c2264abc72a6c3e23d61caa00b10cedd2794f96d
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-28 20:17:56 +0300

    M101J: Spark is plugged.

---
+++ M101J/week-1/FirstMavenProject/src/main/java/com/mongodb/HelloWorldSparkStyle.java
@@ -0,0 +1,20 @@
+package com.mongodb;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+/**
+ * Created by bozhin on 5/28/16.
+ */
+public class HelloWorldSparkStyle {
+    public static void main(String[] args) {
+        Spark.get(new Route("/") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World From Spark.";
+            }
+        });
+    }
+}
