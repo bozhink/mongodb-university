9e312420dc8125578f6fd164750a31f629e8cec5
Bozhin Karaivanov <bozhink@gmail.com>
2016-06-12 12:44:03 +0300

    M101J: homework 3.3 is done

--- M101J/week-3/homework/blog_accepting_posts_and_comments/src/main/java/course/BlogPostDAO.java
+++ M101J/week-3/homework/blog_accepting_posts_and_comments/src/main/java/course/BlogPostDAO.java
@@ -41,8 +41,11 @@ public class BlogPostDAO {
         return posts;
     }
 
-
-    public String addPost(String title, String body, List tags, String username) {
+    public String addPost(
+            String title,
+            String body,
+            List tags,
+            String username) {
 
         System.out.println("inserting blog entry " + title + " " + body);
 
@@ -84,13 +87,31 @@ public class BlogPostDAO {
 
 
     // Append a comment to a blog post
-    public void addPostComment(final String name, final String email, final String body,
-                               final String permalink) {
+    public void addPostComment(
+            final String name,
+            final String email,
+            final String body,
+            final String permalink) {
 
         // XXX HW 3.3, Work Here
         // Hints:
         // - email is optional and may come in NULL. Check for that.
         // - best solution uses an update command to the database and a suitable
         //   operator to append the comment on to any existing list of comments
+
+        Document comment = new Document("author", name);
+        if (email != null && !email.trim().equals("")) {
+            comment = comment.append("email", email);
+        }
+
+        comment = comment.append("body", body);
+
+        Document filter = new Document("permalink", permalink);
+        Document update = new Document("$push", new Document("comments", comment));
+        try {
+            this.postsCollection.updateOne(filter, update);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
